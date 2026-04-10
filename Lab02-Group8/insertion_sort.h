6c7c1daba29afb55bc1f152be7c4afaeccee76ee
Hassnain-cs <mhassnain3547@conestogac.on.ca>
2026-04-09 21:41:59 -0400

    Function declarations for insertion sort module

--- Lab02-Group8/insertion_sort.h
+++ Lab02-Group8/insertion_sort.h
@@ -1,1 +1,16 @@
 #pragma once
+#ifndef INSERTION_SORT_H
+#define INSERTION_SORT_H
+
+/*
+    Adding a separate header file to 
+    keep the code organised and modular.
+*/
+
+// Sorts a character array using Insertion Sort
+void insertionSort(char arr[], int n);
+
+// Prints the array nicely
+void printArray(char arr[], int n);
+
+#endif
